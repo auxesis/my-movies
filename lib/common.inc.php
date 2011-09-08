03c49ae63904c6585533a3217ba47e414b24de46
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-09-08 10:27:15 +1000

    Fix whitespace

--- lib/common.inc.php
+++ lib/common.inc.php
@@ -2,33 +2,33 @@
 
 function mysql_query_wrapper($query) {
 	global $__queries;
-	
+
 	/*
 	 This is a very light-weight wrapper around mysql_query() that
 	 understands:
-	 
+
 	 - The performance instrumentation class
 	 - Writing to a debug log if a query was invalid.
 	 - Logging all queries.  If debug mode is on, the logged
 	   queries display at the end of the page.
- 
+
 	*/
-	
+
 	if (!isset($__queries)) $__queries = array();
-	
+
 	$__queries[] = $query;
 
 	$return = MySQL_perf::mysql_query($query);
-	
+
 	if (!$return) {
 		debug_write($query);
 	}
-	
+
 	return $return;
 }
 
 function debug_write($string) {
-	
+
 	if (DEBUG_MODE) {
 		print $string;
 	} else {
@@ -51,7 +51,7 @@ function get_number_of_actors() {
 
 function get_random_movie() {
 
-	$result = mysql_query_wrapper("SELECT * FROM title WHERE title != '' 
+	$result = mysql_query_wrapper("SELECT * FROM title WHERE title != ''
 	AND kind_id = 1
 	ORDER BY RAND() LIMIT 1");
 	return mysql_fetch_assoc($result);
@@ -98,7 +98,7 @@ function get_being_viewed($limit=5) {
 	while($row = mysql_fetch_assoc($result)) {
 		$return[] = $row;
 	}
-	
+
 	return $return;
 }
 
@@ -109,18 +109,18 @@ function get_users_online() {
 	while($row = mysql_fetch_assoc($result)) {
 		$return[] = $row['id'];
 	}
-	
+
 	return $return;
 }
 
 function update_page_views($type, $id) {
-	
+
 	global $me;
-	
-	$my_id = is_object($me) ? $me->id : 0;	
+
+	$my_id = is_object($me) ? $me->id : 0;
 	mysql_query_wrapper("INSERT INTO page_views (type, viewed_id, viewed, user_id)
 		VALUES ('$type', $id, NOW(), $my_id)") or mysql_error();
-	
+
 
 }
 
@@ -149,7 +149,7 @@ function create_new_user($first_name, $last_name, $email) {
 		mysql_real_escape_string($last_name),
 		mysql_real_escape_string($email)
 	));
-	
+
 	return mysql_insert_id();
 
 }
